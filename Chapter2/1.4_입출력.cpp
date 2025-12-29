c04c6b9e253b854f408ab27a91ea8d7146093c85
caelumean215 <98576510+caelumean@users.noreply.github.com>
2025-12-29 22:58:07 +0900

    [Update] 1.2~1.4

---
+++ Chapter2/1.4_입출력.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+	// std라는 이름 공간 안에 들어있는 cout을 사용하기 위해서 ::(콜론콜론)을 중간에 넣어준다
+	std::cout << "I love this lecture!!!!" << std::endl;
+
+	std::cout << "Hello World" << std::endl;
+	return 0;
+}
