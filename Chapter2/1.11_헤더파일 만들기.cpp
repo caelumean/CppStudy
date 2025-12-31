fcc9fee3fe741f631f6114887c0ea5c9cbdf017a
caelumean215 <98576510+caelumean@users.noreply.github.com>
2025-12-31 22:10:43 +0900

    [Update]1.11

---
+++ Chapter2/1.11_헤더파일 만들기.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Myheaders/1.11.1_add.h"
+// 헤더파일을 나중에 많이 만들게 될 것인데
+// 파일로 묶고 정리하는 걸 잘 해줘야한다
+
+using namespace std;
+
+// 헤더파일
+// 다시 재사용하기 위해서 만든다
+// 1. 1.11.1_add.cpp 만들기
+// 2. 함수 기능을 만들기
+// 3. 1.11.1_add.h 헤더 파일을 만들기
+// 4. int add(int a, int b); -> 선언하기
+// 5. #include "1.11.1_add.h" -> 인클루드 해주기
+
+
+int main()
+{
+
+	cout << add(1, 2) << endl;
+
+	return 0;
+}
