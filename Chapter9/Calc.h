d7f58899d18d4946691972b0737e3dd62a0a559d
caelumean215 <98576510+caelumean@users.noreply.github.com>
2026-02-11 22:30:05 +0900

    [update] 8.8

--- Chapter9/Calc.h
+++ Chapter9/Calc.h
@@ -1,5 +1,18 @@
 #pragma once
+#include <iostream>
+
+// 헤더 파일에서 using namespace를 하게 되면 전부 영향을 받게 되어서
+// 안쓰는 것이 좋다
 class Calc
 {
+private:
+	int m_value;
+
+public:
+	Calc(int init_value);
+	Calc& add(int value);
+	Calc& sub(int value);
+	Calc& mult(int value);
+	void print1();
 };
 
