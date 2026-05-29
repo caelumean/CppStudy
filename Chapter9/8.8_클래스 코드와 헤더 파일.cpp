73447324a378153d04ce4feb4068343efb21b64a
LEEJIWON <98576510+caelumean@users.noreply.github.com>
2026-05-29 13:42:17 +0900

    [Update]

--- Chapter9/8.8_클래스 코드와 헤더 파일.cpp
+++ Chapter9/8.8_클래스 코드와 헤더 파일.cpp
@@ -3,10 +3,23 @@
 h파일을 만들어서 선언과 정의를 분리해서 만든다.
 
 */
+#include <iostream>
+
+int solution(int a, int b, bool flag)
+{
+	return flag ? a + b : a - b;
+}
 
 int main()
 {
 
+	int a = 0;
+	int b = 0;
+	bool flag = true;
+
+	std::cout << "숫자 두개와 flag값을 입력하시오" << std::endl;
+	std::cin >> a >> b >> flag;
+	std::cout << solution(a, b, flag) << std::endl;
 
 	return 0;
 }
